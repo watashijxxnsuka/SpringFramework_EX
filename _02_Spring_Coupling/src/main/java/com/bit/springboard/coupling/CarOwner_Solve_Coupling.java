7c43c8c21e244fa38dd74e454121f9d17c45d0a9
WATASHIJXXNSUKA\shoon <jxxn2538@gmail.com>
2024-07-09 11:50:19 +0900

    24.07.09

--- _02_Spring_Coupling/src/main/java/com/bit/springboard/coupling/CarOwner_Solve_Coupling.java
+++ _02_Spring_Coupling/src/main/java/com/bit/springboard/coupling/CarOwner_Solve_Coupling.java
@@ -1,2 +1,20 @@
-package com.bit.springboard.coupling;public class CarOwner_Solve_Coupling {
+package com.bit.springboard.coupling;
+
+public class CarOwner_Solve_Coupling {
+    public static void main(String[] args) {
+        // 인터페이스 상속을 통한 다형성 구축을 통해 결합도를 낮추는 방식
+        Car car = new KiaCar();
+
+        car.engineOn();
+        car.speedUp();
+        car.speedDown();
+        car.engineOff();
+
+        car = new HyundaiCar();
+
+        car.engineOn();
+        car.speedUp();
+        car.speedDown();
+        car.engineOff();
+    }
 }
---
+++ _02_Spring_Coupling/src/main/webapp/index_old.jsp
@@ -0,0 +1,8 @@
+<!-- jsp 선언문-->
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+    <body>
+        <h2>내 이름은 한서준이에요.</h2>
+        <img src="${pageContext.request.contextPath}/images/bit.png">
+    </body>
+</html>
